b6147e76bb28b60c14fe5a67e92fe189632634a2
acmgit <undertakers_help@yahoo.com>
2022-02-07 04:23:02 +0100

    Seems to be all

--- register_mods.lua
+++ register_mods.lua
@@ -3,10 +3,10 @@ local mf = moreflowers
 
 if(minetest.get_modpath("hunger_ng")) then
     local add = hunger_ng.add_hunger_data
-    
+
     for key, value in pairs(mf.mushroomdatas) do
             add(modname .. ":" .. value[1], {satiates = value[4], heal = value[5]})
-            
+
     end -- for key
-    
+
 end -- if(minetest.get_modname("hunger_ng
