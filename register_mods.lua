8e8ed9ef2563acf89aa3bb62e141b09abd5b6472
acmgit <undertakers_help@yahoo.com>
2022-02-08 08:56:20 +0100

    Same Proceedere as every Bug

--- register_mods.lua
+++ register_mods.lua
@@ -6,7 +6,7 @@ if(minetest.get_modpath("hunger_ng")) then
 
     for key, value in pairs(mf.mushroomdatas) do
             add(modname .. ":" .. value[1], {satiates = value[4], heal = value[5]})
-            minetest.log("action",mf.modname .. " registers " .. modname.value[1] .. " @hunger_ng.")
+            minetest.log("action",mf.modname .. " registers " .. modname.. ":" .. value[1] .. " @hunger_ng.")
     end -- for key
 
 end -- if(minetest.get_modname("hunger_ng
@@ -16,13 +16,13 @@ if(minetest.get_modpath("techage")) then
 
     for key, value in pairs(mf.mushroomdatas) do
         reg(modname .. ":" .. value[1])
-        minetest.log("action",mf.modname .. " registers " .. modname.value[1] .. " @techage.")
+        minetest.log("action",mf.modname .. " registers " .. modname.. ":" .. value[1] .. " @techage.")
 
     end
 
     for key, value in pairs(mf.flowerdatas) do
         reg(modname .. ":" .. value[1])
-        minetest.log("action",mf.modname .. " registers " .. modname.value[1] .. " @techage.")
+        minetest.log("action",mf.modname .. " registers " .. modname..":" .. value[1] .. " @techage.")
     end
 
 end
